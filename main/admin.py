64794372b779b58e11f1a911a20745e220bde891
TheoVasile <theo.vasile50@yahoo.com>
2019-05-19 15:03:18 -0400

    implemented text editor

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Article
+from tinymce.widgets import TinyMCE
+from django.db import models
+
+class AdminArticle(admin.ModelAdmin):
+    fieldsets = [
+        ('title/date', {'fields':['title','published']}),
+        ('content', {'fields':['content']})
+    ]
+
+    formfield_overrides = {
+        models.TextField:{'widget':TinyMCE()}
+    }
 
 # Register your models here.
+admin.site.register(Article, AdminArticle)
--- main/models.py
+++ main/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    published = models.DateTimeField("date published")
+    published = models.DateTimeField("date published", default=datetime.now())
 
     def __str__(self):
         return self.title
