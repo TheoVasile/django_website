aafae437a01fb9611005f94a0783f534911ad12e
TheoVasile <theo.vasile50@yahoo.com>
2019-05-20 13:47:15 -0400

    added error messages

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Article
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 # Create your views here.
 def homepage(request):
@@ -14,11 +15,13 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"congrats {username}!")
             login(request, user)
             return redirect("main:homepage")
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
     form = UserCreationForm
     return render(request,
