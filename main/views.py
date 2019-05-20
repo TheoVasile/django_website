870a9c7aca1f9c186d82a3fd19f5c8016a60fb31
TheoVasile <theo.vasile50@yahoo.com>
2019-05-20 10:49:54 -0400

    display articles on homepage

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse("wow <strong> FANTASTIC </strong>")
+    return render(request=request,
+                  template_name="main/home.html",
+                  context={"articles":Article.objects.all})
